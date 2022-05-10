e211a4a540a60ff1fb0fdebd0c9403f419590bfd
Beatriz <ana-beatrizb10@hotmail.com>
2022-05-10 09:16:07 -0300

    Primeira casse: Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,12 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Cu");
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 
 	}
 
