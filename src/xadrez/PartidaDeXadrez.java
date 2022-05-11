a955a6ee269930c081628bc5038c4c222978b4dd
Beatriz <ana-beatrizb10@hotmail.com>
2022-05-11 18:49:44 -0300

    Cavalo

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -8,6 +8,7 @@ import tabuleirojogo.Peca;
 import tabuleirojogo.Posicao;
 import tabuleirojogo.Tabuleiro;
 import xadrez.pecas.Bispo;
+import xadrez.pecas.Cavalo;
 import xadrez.pecas.Piao;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -192,9 +193,11 @@ public class PartidaDeXadrez {
 	
 	private void ConfigInicial() {
 		colocarNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
-		colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
+		colocarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('a', 2, new Piao(tabuleiro, Cor.BRANCO));
 		colocarNovaPeca('b', 2, new Piao(tabuleiro, Cor.BRANCO));
@@ -206,9 +209,11 @@ public class PartidaDeXadrez {
 		colocarNovaPeca('h', 2, new Piao(tabuleiro, Cor.BRANCO));
 		
 		colocarNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
-		colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
+		colocarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('a', 7, new Piao(tabuleiro, Cor.PRETO));
 		colocarNovaPeca('b', 7, new Piao(tabuleiro, Cor.PRETO));
---
+++ src/xadrez/pecas/Cavalo.java
@@ -0,0 +1,73 @@
+package xadrez.pecas;
+
+import tabuleirojogo.Posicao;
+import tabuleirojogo.Tabuleiro;
+import xadrez.Cor;
+import xadrez.PecaDeXadrez;
+
+public class Cavalo extends PecaDeXadrez{
+
+	public Cavalo(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro, cor);
+	}
+
+	@Override
+	public String toString() {
+		return "C";
+	}
+	
+	private boolean podeMoverR(Posicao posicao) {
+		PecaDeXadrez p = (PecaDeXadrez) getTabuleiro().peca(posicao);
+		return p == null || p.getCor() != getCor();
+	}
+	
+	@Override
+	public boolean[][] possiveisMovimentos() {
+		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Posicao p = new Posicao(0, 0);
+
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 2);
+		if (getTabuleiro().posicaoExiste(p) && podeMoverR(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 2);
+		if (getTabuleiro().posicaoExiste(p) && podeMoverR(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValores(posicao.getLinha() - 2, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(p) && podeMoverR(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValores(posicao.getLinha() - 2, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(p) && podeMoverR(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 2);
+		if (getTabuleiro().posicaoExiste(p) && podeMoverR(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 2);
+		if (getTabuleiro().posicaoExiste(p) && podeMoverR(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValores(posicao.getLinha() + 2, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(p) && podeMoverR(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValores(posicao.getLinha() + 2, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(p) && podeMoverR(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		return mat;
+	}
+
+}
